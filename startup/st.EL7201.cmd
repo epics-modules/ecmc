3fc96aa02563f9f662153990724afd49af5eec97
Torsten Bögershausen <torsten.bogershausen@esss.se>
2017-01-16 12:15:30 +0100

    Use axis instead of motor

--- startup/st.EL7201.cmd
+++ startup/st.EL7201.cmd
@@ -1,6 +1,6 @@
 require asyn
 require streamdevice
-require motor,6.10.3-ESS
+require axis,10.0.1
 require ecmc
 
 ## Configure devices
@@ -31,33 +31,33 @@ ecmcCreateController("MCU1", "MC_CPU1", "32", "200", "1000", "")
 # 5. Fhex  (Enable operation)
 #
 #
-ecmcConfigController "MCU1", "Cfg.SetEnableFuncCallDiag(0)"
-ecmcConfigController "MCU1", "Cfg.EcSetMaster(0)"
+EthercatMCConfigController "MCU1", "Cfg.SetEnableFuncCallDiag(0)"
+EthercatMCConfigController "MCU1", "Cfg.EcSetMaster(0)"
 
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(2,0x2,0x13ed3052,1,2,0x1600,0x7000,0x01,8,CONTROL)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(2,0x2,0x13ed3052,1,2,0x1600,0x7000,0x02,16,VALUE)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(2,0x2,0x13ed3052,2,3,0x1a03,0x6010,0x00,16,ENC_STATUS)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(2,0x2,0x13ed3052,2,3,0x1a03,0x6010,0x10,16,POSITION)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(2,0x2,0x13ed3052,2,3,0x1a03,0x6010,0x20,16,LATCH_POSITION)"
-ecmcConfigController "MCU1", "Cfg.EcSlaveConfigDC(2,0x320,1000000,500000,1000000,500000)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(2,0x2,0x13ed3052,1,2,0x1600,0x7000,0x01,8,CONTROL)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(2,0x2,0x13ed3052,1,2,0x1600,0x7000,0x02,16,VALUE)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(2,0x2,0x13ed3052,2,3,0x1a03,0x6010,0x00,16,ENC_STATUS)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(2,0x2,0x13ed3052,2,3,0x1a03,0x6010,0x10,16,POSITION)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(2,0x2,0x13ed3052,2,3,0x1a03,0x6010,0x20,16,LATCH_POSITION)"
+EthercatMCConfigController "MCU1", "Cfg.EcSlaveConfigDC(2,0x320,1000000,500000,1000000,500000)"
 
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(7,0x2,0x1b7d3052,1,2,0x1602,0x7010,0x1,16,STM_CONTROL)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(7,0x2,0x1b7d3052,1,2,0x1604,0x7010,0x21,16,VELOCITY_SETPOINT)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(7,0x2,0x1b7d3052,2,3,0x1a00,0x6000,0x0,16,ENC_STATUS)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(7,0x2,0x1b7d3052,2,3,0x1a00,0x6000,0x11,16,POSITION)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(7,0x2,0x1b7d3052,2,3,0x1a00,0x6000,0x12,16,LATCH_POSITION)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(7,0x2,0x1b7d3052,2,3,0x1a03,0x6010,0x1,16,STM_STATUS)"
-ecmcConfigController "MCU1", "Cfg.EcSlaveConfigDC(7,0x300,1000000,20,1000000,20)"
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(7,0x8010,0x1,1000,2)"
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(7,0x8012,0x5,1,1)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(7,0x2,0x1b7d3052,1,2,0x1602,0x7010,0x1,16,STM_CONTROL)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(7,0x2,0x1b7d3052,1,2,0x1604,0x7010,0x21,16,VELOCITY_SETPOINT)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(7,0x2,0x1b7d3052,2,3,0x1a00,0x6000,0x0,16,ENC_STATUS)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(7,0x2,0x1b7d3052,2,3,0x1a00,0x6000,0x11,16,POSITION)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(7,0x2,0x1b7d3052,2,3,0x1a00,0x6000,0x12,16,LATCH_POSITION)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(7,0x2,0x1b7d3052,2,3,0x1a03,0x6010,0x1,16,STM_STATUS)"
+EthercatMCConfigController "MCU1", "Cfg.EcSlaveConfigDC(7,0x300,1000000,20,1000000,20)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(7,0x8010,0x1,1000,2)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(7,0x8012,0x5,1,1)"
 #Sync mode 8010:19
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(7,0x1C32,0x1,2,2)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(7,0x1C32,0x1,2,2)"
 #Cycle time
-#ecmcConfigController "MCU1", "Cfg.EcAddSdo(7,0x1C32,0x2,1000000,4)"
+#EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(7,0x1C32,0x2,1000000,4)"
 #Sync mode 
-#ecmcConfigController "MCU1", "Cfg.EcAddSdo(7,0x1C33,0x1,2,2)"
+#EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(7,0x1C33,0x1,2,2)"
 #Cycle time
-#ecmcConfigController "MCU1", "Cfg.EcAddSdo(7,0x1C33,0x2,1000000,4)"
+#EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(7,0x1C33,0x2,1000000,4)"
 
 
 
@@ -65,106 +65,106 @@ ecmcConfigController "MCU1", "Cfg.EcAddSdo(7,0x1C32,0x1,2,2)"
 
 
 #Test Resolver input
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(9,0x2,0x1c213052,1,2,0x1600,0x7010,0x01,16,CONTROL)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(9,0x2,0x1c213052,1,2,0x1601,0x7010,0x06,32,VELOCITY_SETPOINT)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(9,0x2,0x1c213052,2,3,0x1a00,0x6000,0x11,32,POSITION)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(9,0x2,0x1c213052,2,3,0x1a01,0x6010,0x01,16,STATUS)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(9,0x2,0x1c213052,1,2,0x1600,0x7010,0x01,16,CONTROL)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(9,0x2,0x1c213052,1,2,0x1601,0x7010,0x06,32,VELOCITY_SETPOINT)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(9,0x2,0x1c213052,2,3,0x1a00,0x6000,0x11,32,POSITION)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(9,0x2,0x1c213052,2,3,0x1a01,0x6010,0x01,16,STATUS)"
 #NOTE important the sync 1 period needs to be 0!
-ecmcConfigController "MCU1", "Cfg.EcSlaveConfigDC(9,0x700,1000000,500000,0,0)"
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(9,0x8011,0x11,1000,4)"
-ecmcConfigController "MCU1", "Cfg.EcSlaveConfigWatchDog(9,1000,10000)"
+EthercatMCConfigController "MCU1", "Cfg.EcSlaveConfigDC(9,0x700,1000000,500000,0,0)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(9,0x8011,0x11,1000,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcSlaveConfigWatchDog(9,1000,10000)"
 
 #*************** Distributed clocks config EL7201
 #Sync mode 
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(9,0x1C32,0x1,3,2)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(9,0x1C32,0x1,3,2)"
 #Cycle time
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(9,0x1C32,0x2,1000000,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(9,0x1C32,0x2,1000000,4)"
 #Sync mode 
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(9,0x1C33,0x1,3,2)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(9,0x1C33,0x1,3,2)"
 #Cycle time
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(9,0x1C33,0x2,1000000,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(9,0x1C33,0x2,1000000,4)"
 
 #Does not work. DC-patch needed...
-ecmcConfigController "MCU1", "Cfg.EcSelectReferenceDC(0,7)"
+EthercatMCConfigController "MCU1", "Cfg.EcSelectReferenceDC(0,7)"
 
 #***************Parmetrization of EL7201
 #Nominal voltage = 48V
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(9,0x8010,0x19,48000,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(9,0x8010,0x19,48000,4)"
 
 #Motor max current = 6A
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(9,0x8011,0x11,6000,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(9,0x8011,0x11,6000,4)"
 
 #Motor rated current = 4A
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(9,0x8011,0x12,4000,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(9,0x8011,0x12,4000,4)"
 
 #Motor pole pairs = 3
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(9,0x8011,0x13,3,1)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(9,0x8011,0x13,3,1)"
 
-ecmcConfigController "MCU1", "Cfg.EcApplyConfig(1)"
+EthercatMCConfigController "MCU1", "Cfg.EcApplyConfig(1)"
 
-ecmcConfigController "MCU1", "Cfg.CreateDefaultAxis(1)"
+EthercatMCConfigController "MCU1", "Cfg.CreateDefaultAxis(1)"
 #Set DS402 drive type
-ecmcConfigController "MCU1", "Cfg.SetAxisDrvType(1,1)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisDrvType(1,1)"
 
-ecmcConfigController "MCU1", "Main.M1.fAcceleration=1000"
-ecmcConfigController "MCU1", "Main.M1.fDeceleration=1000"
-ecmcConfigController "MCU1", "Cfg.SetAxisEmergDeceleration(1,200)"
-ecmcConfigController "MCU1", "Main.M1.fVelocity=3600"
-ecmcConfigController "MCU1", "Main.M1.nCommand=1"
-ecmcConfigController "MCU1", "Cfg.SetAxisJogVel(1,100.0)"
-ecmcConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#D,8,5=-5000"
-ecmcConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#E,8,5=5000"
-ecmcConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#B,2,2=0"
-ecmcConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#C,2,2=0"
-ecmcConfigController "MCU1", "ADSPORT=501/.ADR.16#4001,16#6,8,5=10"
-ecmcConfigController "MCU1", "ADSPORT=501/.ADR.16#4001,16#7,8,5=5"
-ecmcConfigController "MCU1", "Cfg.SetAxisCntrlKp(1,0.8)"
-ecmcConfigController "MCU1", "Cfg.SetAxisCntrlKi(1,0.001)"
-ecmcConfigController "MCU1", "Cfg.SetAxisCntrlKd(1,0)"
-ecmcConfigController "MCU1", "Cfg.SetAxisCntrlKff(1,1)"
-ecmcConfigController "MCU1", "Cfg.SetAxisCntrlOutHL(1,8000.0)"
-ecmcConfigController "MCU1", "Cfg.SetAxisCntrlOutLL(1,-8000.0)"
-ecmcConfigController "MCU1", "Cfg.LinkEcEntryToAxisEncoder(9,POSITION,1,0,-1)"
-ecmcConfigController "MCU1", "Cfg.SetAxisEncScaleDenom(1,1048576)"
-ecmcConfigController "MCU1", "Cfg.SetAxisEncScaleNum(1,360)"
-ecmcConfigController "MCU1", "Cfg.SetAxisEncType(1,0)"
-ecmcConfigController "MCU1", "Cfg.SetAxisEncBits(1,16)"
-ecmcConfigController "MCU1", "Cfg.LinkEcEntryToAxisDrive(9,CONTROL,1,0,-1)"
-ecmcConfigController "MCU1", "Cfg.LinkEcEntryToAxisDrive(9,VELOCITY_SETPOINT,1,1,-1)"
-ecmcConfigController "MCU1", "Cfg.LinkEcEntryToAxisDrive(9,STATUS,1,2,-1)"
+EthercatMCConfigController "MCU1", "Main.M1.fAcceleration=1000"
+EthercatMCConfigController "MCU1", "Main.M1.fDeceleration=1000"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisEmergDeceleration(1,200)"
+EthercatMCConfigController "MCU1", "Main.M1.fVelocity=3600"
+EthercatMCConfigController "MCU1", "Main.M1.nCommand=1"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisJogVel(1,100.0)"
+EthercatMCConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#D,8,5=-5000"
+EthercatMCConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#E,8,5=5000"
+EthercatMCConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#B,2,2=0"
+EthercatMCConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#C,2,2=0"
+EthercatMCConfigController "MCU1", "ADSPORT=501/.ADR.16#4001,16#6,8,5=10"
+EthercatMCConfigController "MCU1", "ADSPORT=501/.ADR.16#4001,16#7,8,5=5"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisCntrlKp(1,0.8)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisCntrlKi(1,0.001)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisCntrlKd(1,0)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisCntrlKff(1,1)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisCntrlOutHL(1,8000.0)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisCntrlOutLL(1,-8000.0)"
+EthercatMCConfigController "MCU1", "Cfg.LinkEcEntryToAxisEncoder(9,POSITION,1,0,-1)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisEncScaleDenom(1,1048576)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisEncScaleNum(1,360)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisEncType(1,0)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisEncBits(1,16)"
+EthercatMCConfigController "MCU1", "Cfg.LinkEcEntryToAxisDrive(9,CONTROL,1,0,-1)"
+EthercatMCConfigController "MCU1", "Cfg.LinkEcEntryToAxisDrive(9,VELOCITY_SETPOINT,1,1,-1)"
+EthercatMCConfigController "MCU1", "Cfg.LinkEcEntryToAxisDrive(9,STATUS,1,2,-1)"
 # 32 bits (+-31 bits)
-ecmcConfigController "MCU1", "Cfg.SetAxisDrvScaleDenom(1,2147483648.0)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisDrvScaleDenom(1,2147483648.0)"
 #Amplifier Max 8000Hz = 2880000 deg/s (8kHz Reverse engineered) 
-ecmcConfigController "MCU1", "Cfg.SetAxisDrvScaleNum(1,2880000.0)"
-ecmcConfigController "MCU1", "Cfg.LinkEcEntryToAxisMonitor(-1,ONE,1,0,0)"
-ecmcConfigController "MCU1", "Cfg.LinkEcEntryToAxisMonitor(-1,ONE,1,1,0)"
-ecmcConfigController "MCU1", "Cfg.LinkEcEntryToAxisMonitor(-1,ONE,1,2,0)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonAtTargetTol(1,0.1)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonAtTargetTime(1,100)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonEnableAtTargetMon(1,1)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonPosLagTol(1,0.5)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonPosLagTime(1,10)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonEnableLagMon(1,0)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonMaxVel(1,5000)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonEnableMaxVel(1,1)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonMaxVelDriveILDelay(1,100)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonMaxVelTrajILDelay(1,200)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonEnableCntrlOutHLMon(1,0)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonCntrlOutHL(1,2100000000)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisDrvScaleNum(1,2880000.0)"
+EthercatMCConfigController "MCU1", "Cfg.LinkEcEntryToAxisMonitor(-1,ONE,1,0,0)"
+EthercatMCConfigController "MCU1", "Cfg.LinkEcEntryToAxisMonitor(-1,ONE,1,1,0)"
+EthercatMCConfigController "MCU1", "Cfg.LinkEcEntryToAxisMonitor(-1,ONE,1,2,0)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonAtTargetTol(1,0.1)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonAtTargetTime(1,100)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonEnableAtTargetMon(1,1)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonPosLagTol(1,0.5)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonPosLagTime(1,10)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonEnableLagMon(1,0)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonMaxVel(1,5000)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonEnableMaxVel(1,1)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonMaxVelDriveILDelay(1,100)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonMaxVelTrajILDelay(1,200)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonEnableCntrlOutHLMon(1,0)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonCntrlOutHL(1,2100000000)"
 
 #Diagnostics
-ecmcConfigController "MCU1", "Cfg.SetEnableFuncCallDiag(0)"
-ecmcConfigController "MCU1", "Cfg.EcSetDomainFailedCyclesLimit(10)"
-ecmcConfigController "MCU1", "Cfg.EcSetDiagnostics(1)"
-ecmcConfigController "MCU1", "Cfg.EcEnablePrintouts(0)"
-ecmcConfigController "MCU1", "Cfg.SetDiagAxisEnable(1)"
-ecmcConfigController "MCU1", "Cfg.SetEnableTimeDiag(0)"
+EthercatMCConfigController "MCU1", "Cfg.SetEnableFuncCallDiag(0)"
+EthercatMCConfigController "MCU1", "Cfg.EcSetDomainFailedCyclesLimit(10)"
+EthercatMCConfigController "MCU1", "Cfg.EcSetDiagnostics(1)"
+EthercatMCConfigController "MCU1", "Cfg.EcEnablePrintouts(0)"
+EthercatMCConfigController "MCU1", "Cfg.SetDiagAxisEnable(1)"
+EthercatMCConfigController "MCU1", "Cfg.SetEnableTimeDiag(0)"
 
 #Enable dirve (except voltage)
-#ecmcConfigController "MCU1", "Cfg.WriteEcEntryIDString(9,CONTROL,7)"
+#EthercatMCConfigController "MCU1", "Cfg.WriteEcEntryIDString(9,CONTROL,7)"
 
 #Go to runtime
-ecmcConfigController "MCU1", "Cfg.SetAppMode(1)"
+EthercatMCConfigController "MCU1", "Cfg.SetAppMode(1)"
 
 
 #############################################################
--- startup/st.KuhnkeFIODriveTest.cmd
+++ startup/st.KuhnkeFIODriveTest.cmd
@@ -1,6 +1,6 @@
 require asyn
 require streamdevice
-require motor,6.10.3-ESS
+require axis,10.0.1
 require ecmc
 
 ## Configure devices
@@ -31,118 +31,118 @@ ecmcCreateController("MCU1", "MC_CPU1", "32", "200", "1000", "")
 # 5. Fhex  (Enable operation)
 #
 #
-ecmcConfigController "MCU1", "Cfg.SetEnableFuncCallDiag(0)"
-ecmcConfigController "MCU1", "Cfg.EcSetMaster(0)"
+EthercatMCConfigController "MCU1", "Cfg.SetEnableFuncCallDiag(0)"
+EthercatMCConfigController "MCU1", "Cfg.EcSetMaster(0)"
 
 #Kuhnke drive at slave position 11
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,1,2,0x1600,0x6040,0x0,16,STM_CONTROL)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,1,2,0x1600,0x607A,0x0,32,TARGET_POSITION)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,1,2,0x1600,0x3202,0x0,32,SUB_MODE_SELECT)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,1,2,0x1600,0x6060,0x0,8,OP_MODE_CMD)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,1,2,0x1600,0x6040,0x0,16,STM_CONTROL)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,1,2,0x1600,0x607A,0x0,32,TARGET_POSITION)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,1,2,0x1600,0x3202,0x0,32,SUB_MODE_SELECT)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,1,2,0x1600,0x6060,0x0,8,OP_MODE_CMD)"
 
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,1,2,0x1603,0x6043,0x0,16,VELOCITY_SETPOINT)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,1,2,0x1603,0x6043,0x0,16,VELOCITY_SETPOINT)"
 
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,2,3,0x1a00,0x6041,0x0,16,STM_STATUS)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,2,3,0x1a00,0x6064,0x0,32,POSITION)"
-ecmcConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,2,3,0x1a00,0x6061,0x0,8,OP_MODE_ACT)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,2,3,0x1a00,0x6041,0x0,16,STM_STATUS)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,2,3,0x1a00,0x6064,0x0,32,POSITION)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddEntryComplete(11,0x0048554b,0x0002ba67,2,3,0x1a00,0x6061,0x0,8,OP_MODE_ACT)"
 
 #Config open loop (expose counter to ethercat bus "Display")
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(11,0x320A,0x3,-1,4)"
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(11,0x320A,0x4,-1,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(11,0x320A,0x3,-1,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(11,0x320A,0x4,-1,4)"
 
 #Open loops steper encoder resolution (51200)
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(11,0x608F,0x1,51200,4)"
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(11,0x608F,0x2,1,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(11,0x608F,0x1,51200,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(11,0x608F,0x2,1,4)"
 
 #Motor Pole pair count (50)
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(11,0x2030,0x0,50,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(11,0x2030,0x0,50,4)"
 
 #Maximal current (3000mA)
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(11,0x2031,0x0,3000,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(11,0x2031,0x0,3000,4)"
 
 #Over voltage limit (51000mV)
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(11,0x2034,0x0,51000,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(11,0x2034,0x0,51000,4)"
 
 #Under voltage limit (45000mV)
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(11,0x2035,0x0,45000,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(11,0x2035,0x0,45000,4)"
 
 #Open loop current reduction at standstill (reduce by 70%=>-70)
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(11,0x2037,0x0,-70,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(11,0x2037,0x0,-70,4)"
 
 #Open loop current reduction at standstill idele time (100ms at stanstill then reduction)
-ecmcConfigController "MCU1", "Cfg.EcAddSdo(11,0x2036,0x0,100,4)"
+EthercatMCConfigController "MCU1", "Cfg.EcAddSdo(11,0x2036,0x0,100,4)"
 
 
 
-ecmcConfigController "MCU1", "Cfg.EcApplyConfig(1)"
+EthercatMCConfigController "MCU1", "Cfg.EcApplyConfig(1)"
 
 #Velocity mode
-ecmcConfigController "MCU1", "Cfg.WriteEcEntryIDString(11,OP_MODE_CMD,2)"
-ecmcConfigController "MCU1", "Cfg.WriteEcEntryIDString(11,STM_CONTROL,128)"
-ecmcConfigController "MCU1", "Cfg.WriteEcEntryIDString(11,SUB_MODE_SELECT,0)"
+EthercatMCConfigController "MCU1", "Cfg.WriteEcEntryIDString(11,OP_MODE_CMD,2)"
+EthercatMCConfigController "MCU1", "Cfg.WriteEcEntryIDString(11,STM_CONTROL,128)"
+EthercatMCConfigController "MCU1", "Cfg.WriteEcEntryIDString(11,SUB_MODE_SELECT,0)"
 
-ecmcConfigController "MCU1", "Cfg.CreateDefaultAxis(1)"
+EthercatMCConfigController "MCU1", "Cfg.CreateDefaultAxis(1)"
 #Set DS402 drive type (must be done right after Create Axis otherwise settings can be lost since new drive object will be created)
-ecmcConfigController "MCU1", "Cfg.SetAxisDrvType(1,1)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisDrvType(1,1)"
 
-ecmcConfigController "MCU1", "Main.M1.fAcceleration=1000"
-ecmcConfigController "MCU1", "Main.M1.fDeceleration=1000"
-ecmcConfigController "MCU1", "Cfg.SetAxisEmergDeceleration(1,200)"
-ecmcConfigController "MCU1", "Main.M1.fVelocity=100"
-ecmcConfigController "MCU1", "Main.M1.nCommand=1"
-ecmcConfigController "MCU1", "Cfg.SetAxisJogVel(1,100.0)"
-ecmcConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#D,8,5=-5000"
-ecmcConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#E,8,5=5000"
-ecmcConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#B,2,2=0"
-ecmcConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#C,2,2=0"
-ecmcConfigController "MCU1", "ADSPORT=501/.ADR.16#4001,16#6,8,5=10"
-ecmcConfigController "MCU1", "ADSPORT=501/.ADR.16#4001,16#7,8,5=5"
-ecmcConfigController "MCU1", "Cfg.SetAxisCntrlKp(1,0.8)"
-ecmcConfigController "MCU1", "Cfg.SetAxisCntrlKi(1,0.001)"
-ecmcConfigController "MCU1", "Cfg.SetAxisCntrlKd(1,0)"
-ecmcConfigController "MCU1", "Cfg.SetAxisCntrlKff(1,1)"
-ecmcConfigController "MCU1", "Cfg.SetAxisCntrlOutHL(1,8000.0)"
-ecmcConfigController "MCU1", "Cfg.SetAxisCntrlOutLL(1,-8000.0)"
-ecmcConfigController "MCU1", "Cfg.LinkEcEntryToAxisEncoder(11,POSITION,1,0,-1)"
-ecmcConfigController "MCU1", "Cfg.SetAxisEncScaleDenom(1,51200)"
-ecmcConfigController "MCU1", "Cfg.SetAxisEncScaleNum(1,360)"
-ecmcConfigController "MCU1", "Cfg.SetAxisEncType(1,0)"
-ecmcConfigController "MCU1", "Cfg.SetAxisEncBits(1,32)"
-ecmcConfigController "MCU1", "Cfg.LinkEcEntryToAxisDrive(11,STM_CONTROL,1,0,-1)"
-ecmcConfigController "MCU1", "Cfg.LinkEcEntryToAxisDrive(11,VELOCITY_SETPOINT,1,1,-1)"
-ecmcConfigController "MCU1", "Cfg.LinkEcEntryToAxisDrive(11,STM_STATUS,1,2,-1)"
-ecmcConfigController "MCU1", "Cfg.SetAxisDrvScaleDenom(1,1.0)"
-ecmcConfigController "MCU1", "Cfg.SetAxisDrvScaleNum(1,1.0)"
-ecmcConfigController "MCU1", "Cfg.LinkEcEntryToAxisMonitor(-1,ONE,1,0,0)"
-ecmcConfigController "MCU1", "Cfg.LinkEcEntryToAxisMonitor(-1,ONE,1,1,0)"
-ecmcConfigController "MCU1", "Cfg.LinkEcEntryToAxisMonitor(-1,ONE,1,2,0)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonAtTargetTol(1,0.1)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonAtTargetTime(1,100)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonEnableAtTargetMon(1,1)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonPosLagTol(1,0.5)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonPosLagTime(1,10)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonEnableLagMon(1,0)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonMaxVel(1,8000)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonEnableMaxVel(1,1)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonMaxVelDriveILDelay(1,100)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonMaxVelTrajILDelay(1,200)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonEnableCntrlOutHLMon(1,0)"
-ecmcConfigController "MCU1", "Cfg.SetAxisMonCntrlOutHL(1,7500)"
+EthercatMCConfigController "MCU1", "Main.M1.fAcceleration=1000"
+EthercatMCConfigController "MCU1", "Main.M1.fDeceleration=1000"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisEmergDeceleration(1,200)"
+EthercatMCConfigController "MCU1", "Main.M1.fVelocity=100"
+EthercatMCConfigController "MCU1", "Main.M1.nCommand=1"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisJogVel(1,100.0)"
+EthercatMCConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#D,8,5=-5000"
+EthercatMCConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#E,8,5=5000"
+EthercatMCConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#B,2,2=0"
+EthercatMCConfigController "MCU1", "ADSPORT=501/.ADR.16#5001,16#C,2,2=0"
+EthercatMCConfigController "MCU1", "ADSPORT=501/.ADR.16#4001,16#6,8,5=10"
+EthercatMCConfigController "MCU1", "ADSPORT=501/.ADR.16#4001,16#7,8,5=5"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisCntrlKp(1,0.8)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisCntrlKi(1,0.001)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisCntrlKd(1,0)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisCntrlKff(1,1)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisCntrlOutHL(1,8000.0)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisCntrlOutLL(1,-8000.0)"
+EthercatMCConfigController "MCU1", "Cfg.LinkEcEntryToAxisEncoder(11,POSITION,1,0,-1)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisEncScaleDenom(1,51200)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisEncScaleNum(1,360)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisEncType(1,0)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisEncBits(1,32)"
+EthercatMCConfigController "MCU1", "Cfg.LinkEcEntryToAxisDrive(11,STM_CONTROL,1,0,-1)"
+EthercatMCConfigController "MCU1", "Cfg.LinkEcEntryToAxisDrive(11,VELOCITY_SETPOINT,1,1,-1)"
+EthercatMCConfigController "MCU1", "Cfg.LinkEcEntryToAxisDrive(11,STM_STATUS,1,2,-1)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisDrvScaleDenom(1,1.0)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisDrvScaleNum(1,1.0)"
+EthercatMCConfigController "MCU1", "Cfg.LinkEcEntryToAxisMonitor(-1,ONE,1,0,0)"
+EthercatMCConfigController "MCU1", "Cfg.LinkEcEntryToAxisMonitor(-1,ONE,1,1,0)"
+EthercatMCConfigController "MCU1", "Cfg.LinkEcEntryToAxisMonitor(-1,ONE,1,2,0)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonAtTargetTol(1,0.1)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonAtTargetTime(1,100)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonEnableAtTargetMon(1,1)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonPosLagTol(1,0.5)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonPosLagTime(1,10)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonEnableLagMon(1,0)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonMaxVel(1,8000)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonEnableMaxVel(1,1)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonMaxVelDriveILDelay(1,100)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonMaxVelTrajILDelay(1,200)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonEnableCntrlOutHLMon(1,0)"
+EthercatMCConfigController "MCU1", "Cfg.SetAxisMonCntrlOutHL(1,7500)"
 
 #Diagnostics
-ecmcConfigController "MCU1", "Cfg.SetEnableFuncCallDiag(0)"
-ecmcConfigController "MCU1", "Cfg.EcSetDomainFailedCyclesLimit(10)"
-ecmcConfigController "MCU1", "Cfg.EcSetDiagnostics(1)"
-ecmcConfigController "MCU1", "Cfg.EcEnablePrintouts(0)"
-ecmcConfigController "MCU1", "Cfg.SetDiagAxisEnable(0)"
-ecmcConfigController "MCU1", "Cfg.SetDiagAxisFreq(1)"
-ecmcConfigController "MCU1", "Cfg.SetEnableTimeDiag(0)"
+EthercatMCConfigController "MCU1", "Cfg.SetEnableFuncCallDiag(0)"
+EthercatMCConfigController "MCU1", "Cfg.EcSetDomainFailedCyclesLimit(10)"
+EthercatMCConfigController "MCU1", "Cfg.EcSetDiagnostics(1)"
+EthercatMCConfigController "MCU1", "Cfg.EcEnablePrintouts(0)"
+EthercatMCConfigController "MCU1", "Cfg.SetDiagAxisEnable(0)"
+EthercatMCConfigController "MCU1", "Cfg.SetDiagAxisFreq(1)"
+EthercatMCConfigController "MCU1", "Cfg.SetEnableTimeDiag(0)"
 
 #Enable dirve (except voltage)
-#ecmcConfigController "MCU1", "Cfg.WriteEcEntryIDString(9,CONTROL,7)"
+#EthercatMCConfigController "MCU1", "Cfg.WriteEcEntryIDString(9,CONTROL,7)"
 
 #Go to runtime
-ecmcConfigController "MCU1", "Cfg.SetAppMode(1)"
+EthercatMCConfigController "MCU1", "Cfg.SetAppMode(1)"
 
 
 #############################################################
