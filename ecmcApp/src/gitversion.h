225352f2c40ced5bbd5c751840fb383c9de5a5ab
Anders Sandström <anders.sandstrom@esss.se>
2017-04-03 13:15:46 +0200

    add missing ecmcApp/src/gitversion.h

---
+++ ecmcApp/src/gitversion.h
@@ -0,0 +1,14 @@
+#ifndef GITVESION_H
+#define GITVESION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const char *git_version_str;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GITVESION_H */
