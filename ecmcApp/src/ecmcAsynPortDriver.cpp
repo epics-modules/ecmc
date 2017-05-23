108d54c111e57f38d976f32acdfd6e6b908c8918
Anders Sandström <anders.sandstrom@esss.se>
2017-05-23 11:09:50 +0200

    ecmcAsynPortDriver: remove example code.

--- ecmcApp/src/ecmcAsynPortDriver.cpp
+++ ecmcApp/src/ecmcAsynPortDriver.cpp
@@ -30,14 +30,6 @@
 #include "ecmcAsynPortDriver.h"
 #include <epicsExport.h>
 
-#define FREQUENCY 1000       /* Frequency in Hz */
-#define AMPLITUDE 1.0        /* Plus and minus peaks of sin wave */
-#define NUM_DIVISIONS 10     /* Number of scope divisions in X and Y */
-#define MIN_UPDATE_TIME 0.02 /* Minimum update time, to prevent CPU saturation */
-
-#define MAX_ENUM_STRING_SIZE 20
-static int allVoltsPerDivSelections[NUM_VERT_SELECTIONS]={1,2,5,10};
-
 static const char *driverName="ecmcAsynPortDriver";
 void simTask(void *drvPvt);
 
@@ -45,7 +37,7 @@ void simTask(void *drvPvt);
   * Calls constructor for the asynPortDriver base class.
   * \param[in] portName The name of the asyn port driver to be created.
   * \param[in] maxPoints The maximum  number of points in the volt and time arrays */
-ecmcAsynPortDriver::ecmcAsynPortDriver(const char *portName, int maxPoints,int paramTableSize,int autoConnect)
+ecmcAsynPortDriver::ecmcAsynPortDriver(const char *portName/*, int maxPoints*/,int paramTableSize,int autoConnect)
    : asynPortDriver(portName, 
                     1, /* maxAddr */ 
 		    paramTableSize,
@@ -57,61 +49,8 @@ ecmcAsynPortDriver::ecmcAsynPortDriver(const char *portName, int maxPoints,int p
                     0) /* Default stack size*/    
 {
     asynStatus status;
-    int i;
     const char *functionName = "ecmcAsynPortDriver";
-
-    /* Make sure maxPoints is positive */
-    if (maxPoints < 1) maxPoints = 100;
-    
-    /* Allocate the waveform array */
-    pData_ = (epicsFloat64 *)calloc(maxPoints, sizeof(epicsFloat64));
-
-    /* Allocate the time base array */
-    pTimeBase_ = (epicsFloat64 *)calloc(maxPoints, sizeof(epicsFloat64));
-    /* Set the time base array */
-    for (i=0; i<maxPoints; i++) pTimeBase_[i] = (double)i / (maxPoints-1) * NUM_DIVISIONS;
-    
     eventId_ = epicsEventCreate(epicsEventEmpty);
-    createParam(P_RunString,                asynParamInt32,         &P_Run);
-    createParam(P_MaxPointsString,          asynParamInt32,         &P_MaxPoints);
-    createParam(P_TimePerDivString,         asynParamFloat64,       &P_TimePerDiv);
-    createParam(P_TimePerDivSelectString,   asynParamInt32,         &P_TimePerDivSelect);
-    createParam(P_VertGainString,           asynParamFloat64,       &P_VertGain);
-    createParam(P_VertGainSelectString,     asynParamInt32,         &P_VertGainSelect);
-    createParam(P_VoltsPerDivString,        asynParamFloat64,       &P_VoltsPerDiv);
-    createParam(P_VoltsPerDivSelectString,  asynParamInt32,         &P_VoltsPerDivSelect);
-    createParam(P_VoltOffsetString,         asynParamFloat64,       &P_VoltOffset);
-    createParam(P_TriggerDelayString,       asynParamFloat64,       &P_TriggerDelay);
-    createParam(P_NoiseAmplitudeString,     asynParamFloat64,       &P_NoiseAmplitude);
-    createParam(P_UpdateTimeString,         asynParamFloat64,       &P_UpdateTime);
-    createParam(P_WaveformString,           asynParamFloat64Array,  &P_Waveform);
-    createParam(P_TimeBaseString,           asynParamFloat64Array,  &P_TimeBase);
-    createParam(P_MinValueString,           asynParamFloat64,       &P_MinValue);
-    createParam(P_MaxValueString,           asynParamFloat64,       &P_MaxValue);
-    createParam(P_MeanValueString,          asynParamFloat64,       &P_MeanValue);
-    
-    for (i=0; i<NUM_VERT_SELECTIONS; i++) {
-        // Compute vertical volts per division in mV
-        voltsPerDivValues_[i] = 0;
-        voltsPerDivStrings_[i] = (char *)calloc(MAX_ENUM_STRING_SIZE, sizeof(char));
-        voltsPerDivSeverities_[i] = 0;
-    } 
-
-    /* Set the initial values of some parameters */
-    setIntegerParam(P_MaxPoints,         maxPoints);
-    setIntegerParam(P_Run,               0);
-    setIntegerParam(P_VertGainSelect,    10);
-    setVertGain();
-    setDoubleParam (P_VoltsPerDiv,       1.0);
-    setDoubleParam (P_VoltOffset,        0.0);
-    setDoubleParam (P_TriggerDelay,      0.0);
-    setDoubleParam (P_TimePerDiv,        0.001);
-    setDoubleParam (P_UpdateTime,        0.5);
-    setDoubleParam (P_NoiseAmplitude,    0.1);
-    setDoubleParam (P_MinValue,          0.0);
-    setDoubleParam (P_MaxValue,          0.0);
-    setDoubleParam (P_MeanValue,         0.0);
-    
     
     /* Create the thread that computes the waveforms in the background */
     status = (asynStatus)(epicsThreadCreate("ecmcAsynPortDriverTask",
@@ -125,8 +64,6 @@ ecmcAsynPortDriver::ecmcAsynPortDriver(const char *portName, int maxPoints,int p
     }
 }
 
-
-
 void simTask(void *drvPvt)
 {
     ecmcAsynPortDriver *pPvt = (ecmcAsynPortDriver *)drvPvt;
@@ -143,61 +80,19 @@ void ecmcAsynPortDriver::simTask(void)
 {
     /* This thread computes the waveform and does callbacks with it */
 
-    double timePerDiv, voltsPerDiv, voltOffset, triggerDelay, noiseAmplitude;
-    double updateTime, minValue, maxValue, meanValue;
-    double time, timeStep;
-    double noise, yScale;
-    int run, i, maxPoints;
-    double pi=4.0*atan(1.0);
-    
     lock();
     /* Loop forever */    
     while (1) {
-        getDoubleParam(P_UpdateTime, &updateTime);
-        getIntegerParam(P_Run, &run);
         // Release the lock while we wait for a command to start or wait for updateTime
         unlock();
-        if (run) epicsEventWaitWithTimeout(eventId_, updateTime);
+        if (1) epicsEventWaitWithTimeout(eventId_, 0.5);
         else     (void) epicsEventWait(eventId_);
         // Take the lock again
         lock(); 
-        /* run could have changed while we were waiting */
-        getIntegerParam(P_Run, &run);
-        if (!run) continue;
-        getIntegerParam(P_MaxPoints,        &maxPoints);
-        getDoubleParam (P_TimePerDiv,       &timePerDiv);
-        getDoubleParam (P_VoltsPerDiv,      &voltsPerDiv);
-        getDoubleParam (P_VoltOffset,       &voltOffset);
-        getDoubleParam (P_TriggerDelay,     &triggerDelay);
-        getDoubleParam (P_NoiseAmplitude,   &noiseAmplitude);
-        time = triggerDelay;
-        timeStep = timePerDiv * NUM_DIVISIONS / maxPoints;
-        minValue = 1e6;
-        maxValue = -1e6;
-        meanValue = 0.;
-    
-        yScale = 1.0 / voltsPerDiv;
-        for (i=0; i<maxPoints; i++) {
-            noise = noiseAmplitude * (rand()/(double)RAND_MAX - 0.5);
-            pData_[i] = AMPLITUDE * (sin(time*FREQUENCY*2*pi)) + noise;
-            /* Compute statistics before doing the yOffset and yScale */
-            if (pData_[i] < minValue) minValue = pData_[i];
-            if (pData_[i] > maxValue) maxValue = pData_[i];
-            meanValue += pData_[i];
-            pData_[i] = NUM_DIVISIONS/2 + yScale * (voltOffset + pData_[i]);
-            time += timeStep;
-        }
-        updateTimeStamp();
-        meanValue = meanValue/maxPoints;
-        setDoubleParam(P_MinValue, minValue);
-        setDoubleParam(P_MaxValue, maxValue);
-        setDoubleParam(P_MeanValue, meanValue);
         int tempppp=0;
-        getIntegerParam(17, &tempppp);
-        setIntegerParam(17,tempppp+1);
+        getIntegerParam(0, &tempppp);
+        setIntegerParam(0,tempppp+1);
         callParamCallbacks();
-        doCallbacksFloat64Array(pData_, maxPoints, P_Waveform, 0);
-	//
     }
 }
 
@@ -206,21 +101,21 @@ void ecmcAsynPortDriver::simTask(void)
   * For all parameters it sets the value in the parameter library and calls any registered callbacks..
   * \param[in] pasynUser pasynUser structure that encodes the reason and address.
   * \param[in] value Value to write. */
-asynStatus ecmcAsynPortDriver::writeInt32(asynUser *pasynUser, epicsInt32 value)
+/*asynStatus ecmcAsynPortDriver::writeInt32(asynUser *pasynUser, epicsInt32 value)
 {
     int function = pasynUser->reason;
     asynStatus status = asynSuccess;
     const char *paramName;
     const char* functionName = "writeInt32";
 
-    /* Set the parameter in the parameter library. */
+     Set the parameter in the parameter library.
     status = (asynStatus) setIntegerParam(function, value);
     
-    /* Fetch the parameter string name for possible use in debugging */
+     Fetch the parameter string name for possible use in debugging
     getParamName(function, &paramName);
 
     if (function == P_Run) {
-        /* If run was set then wake up the simulation task */
+         If run was set then wake up the simulation task
         if (value) epicsEventSignal(eventId_);
     } 
     else if (function == P_VertGainSelect) {
@@ -233,11 +128,11 @@ asynStatus ecmcAsynPortDriver::writeInt32(asynUser *pasynUser, epicsInt32 value)
         setTimePerDiv();
     }
     else {
-        /* All other parameters just get set in parameter list, no need to
-         * act on them here */
+         All other parameters just get set in parameter list, no need to
+         * act on them here
     }
     
-    /* Do callbacks so higher layers see any changes */
+     Do callbacks so higher layers see any changes
     status = (asynStatus) callParamCallbacks();
     
     if (status) 
@@ -249,14 +144,14 @@ asynStatus ecmcAsynPortDriver::writeInt32(asynUser *pasynUser, epicsInt32 value)
               "%s:%s: function=%d, name=%s, value=%d\n", 
               driverName, functionName, function, paramName, value);
     return status;
-}
+}*/
 
 /** Called when asyn clients call pasynFloat64->write().
   * This function sends a signal to the simTask thread if the value of P_UpdateTime has changed.
   * For all  parameters it  sets the value in the parameter library and calls any registered callbacks.
   * \param[in] pasynUser pasynUser structure that encodes the reason and address.
   * \param[in] value Value to write. */
-asynStatus ecmcAsynPortDriver::writeFloat64(asynUser *pasynUser, epicsFloat64 value)
+/*asynStatus ecmcAsynPortDriver::writeFloat64(asynUser *pasynUser, epicsFloat64 value)
 {
     int function = pasynUser->reason;
     asynStatus status = asynSuccess;
@@ -264,14 +159,14 @@ asynStatus ecmcAsynPortDriver::writeFloat64(asynUser *pasynUser, epicsFloat64 va
     const char *paramName;
     const char* functionName = "writeFloat64";
 
-    /* Set the parameter in the parameter library. */
+     Set the parameter in the parameter library.
     status = (asynStatus) setDoubleParam(function, value);
 
-    /* Fetch the parameter string name for possible use in debugging */
+     Fetch the parameter string name for possible use in debugging
     getParamName(function, &paramName);
 
     if (function == P_UpdateTime) {
-        /* Make sure the update time is valid. If not change it and put back in parameter library */
+         Make sure the update time is valid. If not change it and put back in parameter library
         if (value < MIN_UPDATE_TIME) {
             asynPrint(pasynUser, ASYN_TRACE_WARNING,
                 "%s:%s: warning, update time too small, changed from %f to %f\n", 
@@ -279,15 +174,15 @@ asynStatus ecmcAsynPortDriver::writeFloat64(asynUser *pasynUser, epicsFloat64 va
             value = MIN_UPDATE_TIME;
             setDoubleParam(P_UpdateTime, value);
         }
-        /* If the update time has changed and we are running then wake up the simulation task */
+         If the update time has changed and we are running then wake up the simulation task
         getIntegerParam(P_Run, &run);
         if (run) epicsEventSignal(eventId_);
     } else {
-        /* All other parameters just get set in parameter list, no need to
-         * act on them here */
+         All other parameters just get set in parameter list, no need to
+         * act on them here
     }
     
-    /* Do callbacks so higher layers see any changes */
+     Do callbacks so higher layers see any changes
     status = (asynStatus) callParamCallbacks();
     
     if (status) 
@@ -299,7 +194,7 @@ asynStatus ecmcAsynPortDriver::writeFloat64(asynUser *pasynUser, epicsFloat64 va
               "%s:%s: function=%d, name=%s, value=%f\n", 
               driverName, functionName, function, paramName, value);
     return status;
-}
+}*/
 
 /** Called when asyn clients call pasynFloat64Array->read().
   * Returns the value of the P_Waveform or P_TimeBase arrays.  
@@ -307,7 +202,7 @@ asynStatus ecmcAsynPortDriver::writeFloat64(asynUser *pasynUser, epicsFloat64 va
   * \param[in] value Pointer to the array to read.
   * \param[in] nElements Number of elements to read.
   * \param[out] nIn Number of elements actually read. */
-asynStatus ecmcAsynPortDriver::readFloat64Array(asynUser *pasynUser, epicsFloat64 *value,
+/*asynStatus ecmcAsynPortDriver::readFloat64Array(asynUser *pasynUser, epicsFloat64 *value,
                                          size_t nElements, size_t *nIn)
 {
     int function = pasynUser->reason;
@@ -338,9 +233,9 @@ asynStatus ecmcAsynPortDriver::readFloat64Array(asynUser *pasynUser, epicsFloat6
               "%s:%s: function=%d\n", 
               driverName, functionName, function);
     return status;
-}
+}*/
     
-asynStatus ecmcAsynPortDriver::readEnum(asynUser *pasynUser, char *strings[], int values[], int severities[], size_t nElements, size_t *nIn)
+/*asynStatus ecmcAsynPortDriver::readEnum(asynUser *pasynUser, char *strings[], int values[], int severities[], size_t nElements, size_t *nIn)
 {
     int function = pasynUser->reason;
     size_t i;
@@ -359,9 +254,9 @@ asynStatus ecmcAsynPortDriver::readEnum(asynUser *pasynUser, char *strings[], in
     }
     *nIn = i;
     return asynSuccess;   
-}
+}*/
 
-void ecmcAsynPortDriver::setVertGain()
+/*void ecmcAsynPortDriver::setVertGain()
 {
     int igain, i;
     double gain;
@@ -375,25 +270,25 @@ void ecmcAsynPortDriver::setVertGain()
         voltsPerDivValues_[i] = (int)(allVoltsPerDivSelections[i] / gain * 1000. + 0.5);
     }
     doCallbacksEnum(voltsPerDivStrings_, voltsPerDivValues_, voltsPerDivSeverities_, NUM_VERT_SELECTIONS, P_VoltsPerDivSelect, 0);
-}
+}*/
 
-void ecmcAsynPortDriver::setVoltsPerDiv()
+/*void ecmcAsynPortDriver::setVoltsPerDiv()
 {
     int mVPerDiv;
     
     // Integer volts are in mV
     getIntegerParam(P_VoltsPerDivSelect, &mVPerDiv);
     setDoubleParam(P_VoltsPerDiv, mVPerDiv / 1000.);
-}
+}*/
 
-void ecmcAsynPortDriver::setTimePerDiv()
+/*void ecmcAsynPortDriver::setTimePerDiv()
 {
     int microSecPerDiv;
     
     // Integer times are in microseconds
     getIntegerParam(P_TimePerDivSelect, &microSecPerDiv);
     setDoubleParam(P_TimePerDiv, microSecPerDiv / 1000000.);
-}
+}*/
 
 /* Configuration routine.  Called directly, or from the iocsh function below */
 
@@ -411,7 +306,7 @@ int ecmcAsynPortDriverConfigure(const char *portName,int paramTableSize,int auto
 {
   parameterCounter=0;
   maxParameters=paramTableSize;
-  mytestAsynPort=new ecmcAsynPortDriver(portName, 1000,paramTableSize,autoConnect);
+  mytestAsynPort=new ecmcAsynPortDriver(portName,paramTableSize,autoConnect);
   if(mytestAsynPort){
     printf("INFO: New AsynPortDriver success (%s,%i,%i%i).",portName,1000,paramTableSize,autoConnect);
     return(asynSuccess);
--- ecmcApp/src/ecmcAsynPortDriver.h
+++ ecmcApp/src/ecmcAsynPortDriver.h
@@ -14,30 +14,6 @@
 
 #include "asynPortDriver.h"
 
-#define NUM_VERT_SELECTIONS 4
-
-#define MAX_NUM_DYNAMIC_CREATED_PARAMS 1000
-
-/* These are the drvInfo strings that are used to identify the parameters.
- * They are used by asyn clients, including standard asyn device support */
-#define P_RunString                "SCOPE_RUN"                  /* asynInt32,    r/w */
-#define P_MaxPointsString          "SCOPE_MAX_POINTS"           /* asynInt32,    r/o */
-#define P_TimePerDivString         "SCOPE_TIME_PER_DIV"         /* asynFloat64,  r/w */
-#define P_TimePerDivSelectString   "SCOPE_TIME_PER_DIV_SELECT"  /* asynInt32,    r/w */
-#define P_VertGainString           "SCOPE_VERT_GAIN"            /* asynFloat64,  r/w */
-#define P_VertGainSelectString     "SCOPE_VERT_GAIN_SELECT"     /* asynInt32,    r/w */
-#define P_VoltsPerDivString        "SCOPE_VOLTS_PER_DIV"        /* asynFloat64,  r/w */
-#define P_VoltsPerDivSelectString  "SCOPE_VOLTS_PER_DIV_SELECT" /* asynInt32,    r/w */
-#define P_VoltOffsetString         "SCOPE_VOLT_OFFSET"          /* asynFloat64,  r/w */
-#define P_TriggerDelayString       "SCOPE_TRIGGER_DELAY"        /* asynFloat64,  r/w */
-#define P_NoiseAmplitudeString     "SCOPE_NOISE_AMPLITUDE"      /* asynFloat64,  r/w */
-#define P_UpdateTimeString         "SCOPE_UPDATE_TIME"          /* asynFloat64,  r/w */
-#define P_WaveformString           "SCOPE_WAVEFORM"             /* asynFloat64Array,  r/o */
-#define P_TimeBaseString           "SCOPE_TIME_BASE"            /* asynFloat64Array,  r/o */
-#define P_MinValueString           "SCOPE_MIN_VALUE"            /* asynFloat64,  r/o */
-#define P_MaxValueString           "SCOPE_MAX_VALUE"            /* asynFloat64,  r/o */
-#define P_MeanValueString          "SCOPE_MEAN_VALUE"           /* asynFloat64,  r/o */
-
 /** Class that demonstrates the use of the asynPortDriver base class to greatly simplify the task
   * of writing an asyn port driver.
   * This class does a simple simulation of a digital oscilloscope.  It computes a waveform, computes
@@ -46,58 +22,22 @@
   * but they should really all be private. */
 class ecmcAsynPortDriver : public asynPortDriver {
 public:
-    //ecmcAsynPortDriver(const char *portName, int maxArraySize);
-    ecmcAsynPortDriver(const char *portName, int maxPoints,int paramTableSize,int autoConnect);
-                 
+    ecmcAsynPortDriver(const char *portName,int paramTableSize,int autoConnect);
+
     /* These are the methods that we override from asynPortDriver */
-    virtual asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value);
-    virtual asynStatus writeFloat64(asynUser *pasynUser, epicsFloat64 value);
-    virtual asynStatus readFloat64Array(asynUser *pasynUser, epicsFloat64 *value,
-                                        size_t nElements, size_t *nIn);
-    virtual asynStatus readEnum(asynUser *pasynUser, char *strings[], int values[], int severities[],
-                                size_t nElements, size_t *nIn);
+    //virtual asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value);
+    //virtual asynStatus writeFloat64(asynUser *pasynUser, epicsFloat64 value);
+    //virtual asynStatus readFloat64Array(asynUser *pasynUser, epicsFloat64 *value,
+                                        //size_t nElements, size_t *nIn);
+    //virtual asynStatus readEnum(asynUser *pasynUser, char *strings[], int values[], int severities[],
+                                //size_t nElements, size_t *nIn);
 
     /* These are the methods that are new to this class */
     void simTask(void);
 
 protected:
-    /** Values used for pasynUser->reason, and indexes into the parameter library. */
-    int P_Run;
-    #define FIRST_SCOPE_COMMAND P_Run
-    int P_MaxPoints;
-    int P_TimePerDiv;
-    int P_TimePerDivSelect;
-    int P_VertGain;
-    int P_VertGainSelect;
-    int P_VoltsPerDiv;
-    int P_VoltsPerDivSelect;
-    int P_VoltOffset;
-    int P_TriggerDelay;
-    int P_NoiseAmplitude;
-    int P_UpdateTime;
-    int P_Waveform;
-    int P_TimeBase;
-    int P_MinValue;
-    int P_MaxValue;
-    int P_MeanValue;
-    #define LAST_SCOPE_COMMAND P_MeanValue
  
 private:
     /* Our data */
     epicsEventId eventId_;
-    epicsFloat64 *pData_;
-    epicsFloat64 *pTimeBase_;
-    // Actual volts per division are these values divided by vertical gain
-    char *voltsPerDivStrings_[NUM_VERT_SELECTIONS];
-    int voltsPerDivValues_[NUM_VERT_SELECTIONS];
-    int voltsPerDivSeverities_[NUM_VERT_SELECTIONS];
-    void setVertGain();
-    void setVoltsPerDiv();
-    void setTimePerDiv();
-
-    //const char *dynParameterList[MAX_NUM_DYNAMIC_CREATED_PARAMS];
 };
-
-
-#define NUM_SCOPE_PARAMS (&LAST_SCOPE_COMMAND - &FIRST_SCOPE_COMMAND + 1)
-
