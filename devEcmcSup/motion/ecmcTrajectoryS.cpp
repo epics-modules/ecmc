608a08c143f56655502d832ea9e5de1efa31e7bc
Anders Sandstrom <anders.sandstrom@esss.se>
2021-11-26 15:37:25 +0100

    Add s-curve src files (still tapetz source in files WIP)

---
+++ devEcmcSup/motion/ecmcTrajectoryS.cpp
@@ -0,0 +1,410 @@
+/*************************************************************************\
+* Copyright (c) 2019 European Spallation Source ERIC
+* ecmc is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution. 
+*
+*  ecmcTrajectoryS.cpp
+*
+*  Created on: Nov 26, 2021
+*      Author: anderssandstrom
+*
+\*************************************************************************/
+
+#include "ecmcTrajectoryS.h"
+#include <stdio.h>
+
+ecmcTrajectoryS::ecmcTrajectoryS(ecmcAxisData *axisData,
+                                             double        sampleTime) : 
+                       ecmcTrajectoryBase(axisData, sampleTime) {
+  initTraj();
+}
+
+ecmcTrajectoryS::~ecmcTrajectoryS()
+{}
+
+void ecmcTrajectoryS::initVars() {
+  stepACC_                 = 0;
+  stepDEC_                 = 0;
+  stepNOM_                 = 0;
+  stepDECEmerg_            = 0;
+  prevStepSize_            = 0;
+  switchTargetOnTheFly_    = false;
+  stepStableTol_           = 0;
+  thisStepSize_            = 0;
+
+  ecmcTrajectoryBase::initVars();
+}
+
+void ecmcTrajectoryS::initTraj() {
+  printf("ecmcTrajectoryS::initTraj()\n");
+  stepNOM_      = velocityTarget_ * sampleTime_;
+  
+  // TODO check eq.
+  stepACC_      =  acceleration_ * sampleTime_ * sampleTime_  /**2*/;
+  stepDEC_      =  deceleration_ * sampleTime_ * sampleTime_  /**2*/;
+  stepDECEmerg_ =  decelerationEmergency_ * sampleTime_ *
+                  sampleTime_  /**2*/;
+
+  // Avoid rounding errors when comparing doubles
+  if(std::abs(stepACC_) > std::abs(stepDEC_)) {
+    stepStableTol_ = std::abs(stepDEC_/10);
+  } else {
+    stepStableTol_ = std::abs(stepACC_/10);
+  }
+  //ecmcTrajectoryS::initTraj();
+}
+
+void ecmcTrajectoryS::setCurrentPosSet(double posSet) {
+  currentPositionSetpoint_ = posSet;
+  posSetMinus1_            = currentPositionSetpoint_;
+  prevStepSize_            = 0;
+  velocity_                = 0;
+}
+
+double ecmcTrajectoryS::getNextPosSet() {
+  // "Main" of trajectory generator. Needs to be called exactly once per cycle.
+  // Updates trajectory setpoint
+
+  double nextSetpoint = currentPositionSetpoint_;
+  double nextVelocity = velocity_;
+
+  bool stopped = false;
+
+  if (!busy_ || !enable_) {
+    if (execute_ && !enable_) {
+      setErrorID(__FILE__,
+                 __FUNCTION__,
+                 __LINE__,
+                 ERROR_TRAJ_EXECUTE_BUT_NO_ENABLE);
+    }
+    posSetMinus1_ = currentPositionSetpoint_;
+    prevStepSize_ = 0;
+    velocity_     = 0;
+    return currentPositionSetpoint_;
+  }
+  index_++;
+
+  if (!execute_ && (data_->command_.trajSource == ECMC_DATA_SOURCE_INTERNAL)) {
+    data_->interlocks_.noExecuteInterlock = true;
+    data_->refreshInterlocks();
+  }
+
+  nextSetpoint = internalTraj(&nextVelocity);
+  motionDirection nextDir = checkDirection(currentPositionSetpoint_,
+                                           nextSetpoint);
+  // Stop ramp when running external
+  bool externalSourceStopTraj = data_->command_.trajSource !=
+                                ECMC_DATA_SOURCE_INTERNAL;
+
+  if (externalSourceStopTraj ||
+      ((nextDir == ECMC_DIR_BACKWARD) &&
+       data_->interlocks_.trajSummaryInterlockBWD) ||
+      ((nextDir == ECMC_DIR_FORWARD) &&
+       data_->interlocks_.trajSummaryInterlockFWD)) {
+
+    nextSetpoint = moveStop(data_->interlocks_.currStopMode,
+                            currentPositionSetpoint_,
+                            prevStepSize_,
+                            &stopped,
+                            &nextVelocity);
+
+    if (stopped) {
+      velocity_ = 0;
+      busy_         = false;
+      nextVelocity  = 0;
+    }
+  }
+
+  currentPositionSetpoint_ = updateSetpoint(nextSetpoint, nextVelocity);
+
+  return currentPositionSetpoint_;
+}
+
+double ecmcTrajectoryS::updateSetpoint(double nextSetpoint,
+                                             double nextVelocity) {
+  posSetMinus1_            = currentPositionSetpoint_;
+  currentPositionSetpoint_ = checkModuloPos(nextSetpoint,velocityTarget_>0 ? ECMC_DIR_FORWARD:ECMC_DIR_BACKWARD); //=nextSetpoint;
+  prevStepSize_            = thisStepSize_;
+  velocity_                = nextVelocity;
+  distToStop_              = distToStop(velocity_);
+  return currentPositionSetpoint_;
+}
+
+double ecmcTrajectoryS::internalTraj(double *actVelocity) {
+  double posSetTemp = currentPositionSetpoint_;
+
+  switch (motionMode_) {
+  case ECMC_MOVE_MODE_POS:
+    posSetTemp = movePos(currentPositionSetpoint_,
+                          targetPosition_,
+                          distToStop_,
+                          prevStepSize_,
+                          stepNOM_,
+                          &busy_);
+    break;
+  
+  case ECMC_MOVE_MODE_VEL:
+    posSetTemp = moveVel(currentPositionSetpoint_,
+                         prevStepSize_,
+                         stepNOM_,
+                         &busy_);
+    break;
+  }
+  *actVelocity = 0;
+
+  if (busy_) { 
+
+    *actVelocity = thisStepSize_ / sampleTime_; //dist(currentPositionSetpoint_,posSetTemp,velocityTarget_>0 ? ECMC_DIR_FORWARD:ECMC_DIR_BACKWARD) / sampleTime_;
+  }
+  return posSetTemp;
+}
+
+/*
+  *  Method for moving at constant velocity (including ramps)
+  * 
+  * Supports on the fly changes of velocity target setpoint by update of stepNOM.
+  * targetVelo and currVelo can be negative. 
+  * - If decreasing abs(velo) then use decelerartion  (approaching zero velocity)
+  * - If increasing abs(velo) velo then use acceleration
+  * 
+  * Note: 
+  *   prevStepSize: correspons to current velocity
+  *   stepNOM:      position step at velocityTarget 
+  */
+double ecmcTrajectoryS::moveVel(double currSetpoint,
+                                      double prevStepSize,
+                                      double stepNOM,
+                                      bool  *trajBusy) {
+  double positionStep = 0;
+
+  *trajBusy = true;
+  
+   if( prevStepSize > 0) {  // currvelo
+    if(stepNOM > prevStepSize) {
+      // Acc
+      positionStep = prevStepSize + stepACC_;
+    } else if (stepNOM < prevStepSize) {
+      // Dec      
+      positionStep = prevStepSize - stepDEC_;
+    }  else {
+      positionStep = prevStepSize;
+    }
+  } else if ( prevStepSize < 0 ) {
+    if(stepNOM > prevStepSize) {
+      // Dec
+      positionStep = prevStepSize + stepDEC_;
+    } else if(stepNOM < prevStepSize) {
+      // Acc
+      positionStep = prevStepSize - stepACC_;
+    } else {
+      positionStep = prevStepSize;
+    }
+  } else {  // currvelo == 0
+    if(stepNOM > prevStepSize) {
+      // Acc
+      positionStep = prevStepSize + stepACC_;
+    } else if (stepNOM < prevStepSize) {
+      // Acc
+      positionStep = prevStepSize - stepACC_;
+    }
+  }
+
+  // Avoid ripple due to rounding errors
+  double diff = std::abs(stepNOM - positionStep);
+  if( diff < stepStableTol_ ) {
+    positionStep = stepNOM;
+  }
+  
+  thisStepSize_ = positionStep;
+
+  return currSetpoint + thisStepSize_;
+}
+
+/*
+  *  Method for moving to a abs position (including ramps)
+  * 
+  * Supports on the fly changes of velocity target setpoint by update of stepNOM.
+  * Supports on the fly changes of position target setpoint by update of targetSetpoint.
+
+  * targetVelo and currVelo can be negative. 
+  * - If decreasing abs(velo) then use decelerartion  (approaching zero velocity)
+  * - If increasing abs(velo) velo then use acceleration
+  * 
+  * Note: 
+  *   prevStepSize: correspons to current velocity
+  *   stepNOM:      position step at velocityTarget 
+  */ 
+double ecmcTrajectoryS::movePos(double currSetpoint,
+                                      double targetSetpoint,
+                                      double stopDistance,
+                                      double prevStepSize,
+                                      double stepNom,
+                                      bool  *trajBusy) {
+  double posSetTemp   = 0;
+
+  *trajBusy = true;
+
+  // How long to target
+  double distToTargetOld = 0;
+  double distToTargetOldComp = 0;
+  if (prevStepSize > 0) {
+    distToTargetOld = dist(currSetpoint,targetSetpoint, ECMC_DIR_FORWARD);
+    distToTargetOldComp = distToTargetOld - prevStepSize - stepDEC_;
+  } else if (prevStepSize < 0) {
+    distToTargetOld = dist(currSetpoint,targetSetpoint, ECMC_DIR_BACKWARD);
+    distToTargetOldComp = distToTargetOld - prevStepSize + stepDEC_;
+  } else {  // 0 velo, use target
+    distToTargetOld = dist(currSetpoint,targetSetpoint, stepNom > 0 ? ECMC_DIR_FORWARD : ECMC_DIR_BACKWARD);  
+    distToTargetOldComp = distToTargetOld;
+  }
+
+  
+  double distToInitStop = distToTargetOldComp - stopDistance;
+  double stepNomTemp = stepNom;
+
+  if (std::abs(distToInitStop) <= std::abs(prevStepSize_)) {
+    stepNomTemp = 0;  // targetVelo = 0
+  } else if(distToInitStop > 0) {
+    stepNomTemp = std::abs(stepNom);
+  } else {
+    stepNomTemp = -std::abs(stepNom);
+  }
+
+  posSetTemp = moveVel(currSetpoint,
+                       prevStepSize,
+                       stepNomTemp,
+                       trajBusy);
+
+  //double distToTargetNew = dist(posSetTemp,targetSetpoint,prevStepSize >= 0 ? ECMC_DIR_FORWARD : ECMC_DIR_BACKWARD);
+
+  //printf("1 : distToTargetOldComp=%lf,distToTargetOld=%lf,stopDistance=%lf,distToTargetNew=%lf,distToInitStop=%lf,NOM=%lf,DEC=%lf,PREV=%lf\n",distToTargetOldComp,distToTargetOld,stopDistance,distToTargetNew,distToInitStop, stepNOM_,stepDEC_,prevStepSize);
+
+  if( (std::abs(prevStepSize) <= std::abs(stepDEC_)) &&   // The most important!
+      (std::abs(stopDistance) <= 3*std::abs(stepDEC_)) && 
+      (std::abs(distToTargetOld) <= 3*std::abs(stepDEC_))) {
+    posSetTemp      = targetSetpoint;
+    targetPosition_ = posSetTemp;
+    *trajBusy       = false;
+  }
+
+  return posSetTemp;
+}
+
+double ecmcTrajectoryS::moveStop(stopMode stopMode,
+                                       double   currSetpoint,
+                                       double   prevStepSize,                                       
+                                       bool    *stopped,
+                                       double  *velocity) {
+  double positionStep;
+  double posSetTemp = 0;
+  
+  *stopped = false;
+  motionDirection nDir = ECMC_DIR_STANDSTILL;
+
+  if (prevStepSize > 0) {
+    nDir = ECMC_DIR_FORWARD;
+  } else if (prevStepSize < 0) {
+    nDir = ECMC_DIR_BACKWARD;
+  }
+
+  if (stopMode == ECMC_STOP_MODE_EMERGENCY) {
+    positionStep = std::abs(prevStepSize) - stepDECEmerg_;
+  } else {
+    positionStep = std::abs(prevStepSize) - stepDEC_;
+  }
+
+  if (nDir == ECMC_DIR_FORWARD) {
+    posSetTemp = currSetpoint + positionStep;
+    thisStepSize_ = positionStep;
+  } else if (nDir == ECMC_DIR_BACKWARD) {
+    posSetTemp = currSetpoint - positionStep;
+    thisStepSize_ = -positionStep;
+  }
+
+  *velocity = (posSetTemp - currSetpoint) / sampleTime_;
+
+  if ((nDir == ECMC_DIR_STANDSTILL) || (positionStep <= 0)) {
+    *stopped        = true;
+    *velocity       = 0;
+    // To allow for at target monitoring to go high (and then also bBusy)
+    targetPosition_ = currSetpoint;
+    return currSetpoint;
+  }
+
+  return posSetTemp;
+}
+
+/* dist to stop is the triangular area under 
+ *  y = prevStepSize_
+ *  x = std::abs(prevStepSize_/stepDEC_), the number of steps until
+ *      zero speed (if deceleration)
+ */
+double ecmcTrajectoryS::distToStop(double vel) {
+  //double d = 0.5 * vel * vel / deceleration_ + std::abs(vel * sampleTime_);
+  //return vel > 0 ? d : -d ;
+  return prevStepSize_* std::abs(prevStepSize_/stepDEC_) / 2;
+}
+
+void ecmcTrajectoryS::setTargetPos(double pos) {
+  targetPosition_ = pos;
+  index_          = 0;
+
+  // Check if updated on the fly (copied from setExecute().. not so nice)
+  if(busy_ && motionMode_ == ECMC_MOVE_MODE_POS){
+    double dist2Stop=distToStop(velocity_);
+    if(currentPositionSetpoint_ + dist2Stop > targetPosition_) {
+      velocityTarget_= -std::abs(velocityTarget_);
+    }
+    else {
+      velocityTarget_= std::abs(velocityTarget_);
+    }
+  }
+}
+
+void ecmcTrajectoryS::setTargetVel(double velTarget) {
+  velocityTarget_ = velTarget;
+  initTraj();
+}
+
+void ecmcTrajectoryS::setAcc(double acc) {
+  acceleration_ = acc;
+  initTraj();
+}
+
+void ecmcTrajectoryS::setEmergDec(double dec) {
+  decelerationEmergency_ = dec;
+  initTraj();
+}
+
+void ecmcTrajectoryS::setDec(double dec) {
+  deceleration_ = dec;
+
+  if (decelerationEmergency_ == 0) {
+    decelerationEmergency_ = deceleration_ * 3;
+  }
+  initTraj();
+}
+
+// Not used
+void ecmcTrajectoryS::setJerk(double jerk) {
+  jerk_ = jerk;
+  initTraj();
+}
+
+void ecmcTrajectoryS::setEnable(bool enable) {
+  ecmcTrajectoryBase::setEnable(enable);
+  if (!enableOld_ && enable) {
+    prevStepSize_            = 0;
+  }
+}
+
+int ecmcTrajectoryS::initStopRamp(double currentPos,
+                                        double currentVel,
+                                        double currentAcc) {
+  enable_ = 1;
+  busy_                    = true;
+  currentPositionSetpoint_ = currentPos;
+  velocity_                = currentVel;
+  prevStepSize_            = velocity_ * sampleTime_;
+  return 0;
+}
---
+++ devEcmcSup/motion/ecmcTrajectoryS.h
@@ -0,0 +1,115 @@
+/*************************************************************************\
+* Copyright (c) 2019 European Spallation Source ERIC
+* ecmc is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution. 
+*
+*  ecmcTrajectoryS.h
+*
+*  Created on: Nov 26, 2021
+*      Author: anderssandstrom
+*
+\*************************************************************************/
+
+#ifndef SRC_ECMCTRAJECTORYS_H_
+#define SRC_ECMCTRAJECTORYS_H_
+
+#include "ecmcTrajectoryBase.h"
+#include <ruckig>
+
+/**
+ * \class ecmcTrajectoryS
+ *
+ * \ingroup ecmc
+ *
+ * \brief Implements a S-curve motion setpoint trajectory
+ *
+ * This class implements S-curve trajectory nased on based on ruckig sources
+ * Supports:
+ * 1. Constant velocity
+ * 2. Relative positioning
+ * 3. Absolute positioning
+ * 4. Interlocks (hard limits, soft limits, external interlocks)
+ *
+ * Contact: anders.sandstrom@esss.se
+ *
+ * Created on: 2021-11-26
+ *
+ */
+class ecmcTrajectoryS : public ecmcTrajectoryBase {
+ public:
+  ecmcTrajectoryS(ecmcAxisData *axisData,
+                        double        sampleTime);
+  ~ecmcTrajectoryS();
+
+  /** \brief Calculates and returns next position setpoint.
+   * This function should only be executed once for each sample period.
+   */
+  double          getNextPosSet();
+
+   /** \brief Sets target velocity of trajectory (max velocity).
+   * Note: This is the max velocity of the trajectory generator. The actual velocity can be higher.
+   */
+  void            setTargetVel(double velTarget);
+  
+  /// Sets acceleration.
+  void            setAcc(double acc);
+
+  /// Sets deceleration.
+  void            setDec(double dec);
+
+  /** \brief Sets emergency deceleration.
+   * Used for ramp down at hard limits.
+   */
+  void            setEmergDec(double dec);
+
+  /** Currently not implemented (will be used when 
+   * s-shaped trajectory is implemented).
+   */
+  void            setJerk(double jerk);
+
+  /// Sets target position (end position of trajectory).
+  void            setTargetPos(double pos);
+
+  /// Enable traj
+  void            setEnable(bool enable);
+
+  /// Sets position setpoint.
+  void            setCurrentPosSet(double posSet);
+
+  double          distToStop(double vel);
+  int             initStopRamp(double currentPos,
+                               double currentVel,
+                               double currentAcc);
+
+ private:
+  void            initVars();
+  void            initTraj();
+  double          internalTraj(double *velocity);
+  double          moveVel(double currSetpoint,
+                          double prevStepSize,
+                          double stepNom,
+                          bool  *trajBusy);
+  double          movePos(double currSetpoint,
+                          double targetSetpoint,
+                          double stopDistance,
+                          double prevStepSize, // currVelo
+                          double stepNom,   //targetVelo
+                          bool  *trajBusy);
+  double          moveStop(stopMode stopMode,
+                           double   currSetpoint,
+                           double   prevStepSize,
+                           bool    *stopped,
+                           double  *velocity);
+  double          updateSetpoint(double nextSetpoint,
+                                 double nextVelocity);
+
+  double stepACC_;
+  double stepDEC_;
+  double stepNOM_;
+  double stepDECEmerg_;
+  double prevStepSize_;
+  double thisStepSize_;
+  bool switchTargetOnTheFly_;
+  double stepStableTol_;
+};
+#endif  // ifndef SRC_ECMCTRAJECTORYS_H_
